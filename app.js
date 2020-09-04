c421f9a09e1f51ef41878169d15bd3c2a4dcb919
ViniCora <viniciuscoradassi@hotmail.com>
2020-09-04 14:27:35 -0300

    COmmit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { db } from './models/index.js';
+import { gradeRouter } from './routes/gradeRouter.js';
+
+(async () => {
+  try {
+    await db.mongoose
+      .connect(
+        'mongodb+srv://coradassi:1108@cluster0.hr1zc.mongodb.net/Curso?retryWrites=true&w=majority',
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
+      .then(() => {
+        console.log('Banco inicializado');
+      });
+  } catch (error) {
+    process.exit();
+  }
+})();
+
+const app = express();
+
+//define o dominio de origem para consumo do servico
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+  })
+);
+
+app.use(gradeRouter);
+
+app.get('/', (req, res) => {
+  res.send('API em execucao');
+});
+
+app.listen(process.env.PORT || 8081, () => {});
---
+++ controllers/gradeController.js
@@ -0,0 +1,109 @@
+import { db } from '../models/index.js';
+import { logger } from '../config/logger.js';
+import { studentModel } from '../models/studentSchema.js';
+
+const create = async (req, res) => {
+  const grade = req.body;
+  try {
+    const student = new studentModel(grade);
+
+    await student.save();
+    res.send({ message: 'Grade inserido com sucesso' });
+    logger.info(`POST /grade - ${JSON.stringify()}`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+    logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findAll = async (req, res) => {
+  const name = req.query.name;
+
+  //condicao para o filtro no findAll
+  var condition = name
+    ? { name: { $regex: new RegExp(name), $options: 'i' } }
+    : {};
+
+  try {
+    const students = await studentModel.find(condition);
+    logger.info(`GET /grade`);
+    res.send(students);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  const id = req.params.id;
+
+  const student = await studentModel.findById(id);
+
+  res.send(student);
+
+  try {
+    logger.info(`GET /grade - ${id}`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const update = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Dados para atualizacao vazio',
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+    const student = await studentModel.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
+    logger.info(`PUT /grade - ${id} - ${JSON.stringify(req.body)}`);
+    res.send(student);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const student = await studentModel.findByIdAndDelete(id);
+    if (student) {
+      res.send(student);
+    } else {
+      res.status(500).send('Estudante inexistente');
+    }
+    logger.info(`DELETE /grade - ${id}`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const removeAll = async (req, res) => {
+  try {
+    const students = await studentModel.deleteMany({});
+    logger.info(`DELETE /grade`);
+    res.send(students);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+export default { create, findAll, findOne, update, remove, removeAll };
